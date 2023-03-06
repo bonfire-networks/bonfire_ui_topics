21abb77fa087744508e99801cf3ef971ba30e84a
ivanminutillo <ivanminutillo@outlook.it>
2023-03-06 11:22:14 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/488

--- lib/widgets/about/widget_about_live.ex
+++ lib/widgets/about/widget_about_live.ex
@@ -1,11 +1,10 @@
 defmodule Bonfire.UI.Topic.WidgetAboutLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
-  prop title, :string, default: "About this topic"
-  prop about, :string, default: ""
   prop date, :string, default: "Today"
-  prop icon, :string, default: nil
   prop parent, :string, default: nil
   prop parent_link, :string, default: nil
   prop boundary_preset, :any, default: nil
+  prop category, :map, default: nil
+  prop member_count, :integer, default: 0
 end
