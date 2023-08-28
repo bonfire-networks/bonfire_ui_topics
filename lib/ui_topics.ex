0e93cb503bc4266350065f8f07cd68c8bdad19dc
Mayel de Borniol <git@mayel.space>
2023-08-28 15:40:37 +0100

    account

--- lib/ui_topics.ex
+++ lib/ui_topics.ex
@@ -4,13 +4,4 @@ defmodule Bonfire.UI.Topics do
   alias Bonfire.Common.Extend
   alias Bonfire.Common
   alias Common.Types
-
-  def is_admin?(user) do
-    if is_map(user) and Map.get(user, :instance_admin) do
-      Map.get(user.instance_admin, :is_instance_admin)
-    else
-      # FIXME
-      false
-    end
-  end
 end
