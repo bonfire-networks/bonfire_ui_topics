7ee37fd75e1315cb6fbaf082713923955c701019
ivan <ivanminutillo@outlook.it>
2026-03-31 18:34:09 +0200

    groups

---
+++ lib/components/hero/topic_hero_live.ex
@@ -0,0 +1,8 @@
+defmodule Bonfire.UI.Topics.TopicHeroLive do
+  use Bonfire.UI.Common.Web, :stateless_component
+
+  prop category, :map, required: true
+  prop permalink, :string, required: true
+  prop object_boundary, :any, default: nil
+  prop boundary_preset, :any, default: nil
+end
