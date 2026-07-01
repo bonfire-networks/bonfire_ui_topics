00f2172c5fc8fc6ada1bcc6206d51ba09ee56b8f
ivan <ivanminutillo@outlook.it>
2026-07-01 08:54:39 +0200

    theme

--- lib/components/hero/topic_hero_live.ex
+++ lib/components/hero/topic_hero_live.ex
@@ -5,4 +5,7 @@ defmodule Bonfire.UI.Topics.TopicHeroLive do
   prop permalink, :string, required: true
   prop object_boundary, :any, default: nil
   prop boundary_preset, :any, default: nil
+  prop subcategories, :list, default: nil
+  prop select_category_id, :any, default: nil
+  prop user, :map, default: nil
 end
